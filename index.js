b732584e558bd3eca2716dd351beb945db875f50
cs_usha <usc16014@gmail.com>
2024-10-16 11:00:30 +0530

    uploaded music player files

---
+++ index.js
@@ -0,0 +1,57 @@
+$(".english").mouseover(function (){$(".english").css("opacity","0.6");});
+$(".english ").mouseout(function(){$(".english").css("opacity","1");})
+/////////////////////////////////////
+$(".hindi").mouseover(function (){$(".hindi").css("opacity","0.6");});
+$(".hindi ").mouseout(function(){$(".hindi").css("opacity","1");})
+//////////////////////////////////////
+$(".tamil").mouseover(function (){$(".tamil").css("opacity","0.6");});
+$(".tamil ").mouseout(function(){ $(".tamil").css("opacity","1");})
+///////////////////////////////////////
+$(".div0").mouseover(function()
+{
+    $(".div0").css("opacity","0.5");
+})
+$(".div0").mouseout(function()
+{
+    $(".div0").css("opacity","1");
+})
+
+//opacity for image
+$(".pic1").mouseover(function(){$(".pic1").css("opacity","0.5");})
+$(".pic1").mouseout(function(){$(".pic1").css("opacity","1");})
+
+$(".pic2").mouseover(function(){$(".pic2").css("opacity","0.5");})
+$(".pic2").mouseout(function(){$(".pic2").css("opacity","1");})
+
+$(".pic3").mouseover(function(){$(".pic3").css("opacity","0.5");})
+$(".pic3").mouseout(function(){$(".pic3").css("opacity","1");})
+
+$(".pic4").mouseover(function(){$(".pic4").css("opacity","0.5");})
+$(".pic4").mouseout(function(){$(".pic4").css("opacity","1");})
+
+$(".pic5").mouseover(function(){$(".pic5").css("opacity","0.5");})
+$(".pic5").mouseout(function(){$(".pic5").css("opacity","1");})
+
+$(".pic6").mouseover(function(){$(".pic6").css("opacity","0.5");})
+$(".pic6").mouseout(function(){$(".pic6").css("opacity","1");})
+
+$(".pic7").mouseover(function(){$(".pic7").css("opacity","0.5");})
+$(".pic7").mouseout(function(){$(".pic7").css("opacity","1");})
+
+$(".pic8").mouseover(function(){$(".pic8").css("opacity","0.5");})
+$(".pic8").mouseout(function(){$(".pic8").css("opacity","1");})
+
+$(".pic9").mouseover(function(){$(".pic9").css("opacity","0.5");})
+$(".pic9").mouseout(function(){$(".pic9").css("opacity","1");})
+
+
+
+
+    
+
+        
+    
+
+
+    
+
